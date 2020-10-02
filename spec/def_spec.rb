c63f574f9dbf51a8ce6782f1baf694a29cec8eee
Alma <a.q.frankenstein@gmail.com>
2020-10-02 13:02:51 -0700

    fix typos, pass 'all' test

--- spec/def_spec.rb
+++ spec/def_spec.rb
@@ -20,4 +20,14 @@ describe '#Defin' do
     end
   end
 
+  describe('.all') do
+    it("returns a list of definitions for a given word") do
+      defin1 = Defin.new("a northern mammal", @word.id, nil)
+      defin1.save()
+      defin2 = Defin.new("first dog on the moon", @word.id, nil)
+      defin2.save()
+      expect(Defin.all).to(eq([defin1, defin2]))
+    end
+  end
+
 end
