e52cec5afcaea0caf2a24544db70e403985bfb6d
Alma <a.q.frankenstein@gmail.com>
2020-10-02 14:47:09 -0700

    pass integration spec for homepage

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,4 +1,18 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe '#Word' do
+
+  before(:each) do
+    Word.clear()
+  end
+
+  describe('create an initial path', {:type => :feature}) do
+    it('shows homepage') do
+      visit('/')
+      expect(page).to have_content('List')
+    end
+  end
+end
